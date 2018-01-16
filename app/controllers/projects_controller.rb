4ed4435c2ac174da3abe19c36d58d6bf4c0f77b3
Theodore A. Ochieng <royalato@gmail.com>
2018-01-16 00:25:03 +0000

    Added Create and Read functionality for Projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,28 @@
 class ProjectsController < ApplicationController
+    
     def index
+        @projects = Project.order("created_at DESC")
     end
+    
+    def show
+    end
+    
+    #View for the create method
+    def new
+        @project = Project.new
+    end
+    
+    def create
+        @project = Project.new(project_params)
+        @project.save
+        
+        redirect_to root_path
+    end
+    
+    private
+    
+    def project_params
+        params.require(:project).permit(:title, :description)
+    end
+
 end
