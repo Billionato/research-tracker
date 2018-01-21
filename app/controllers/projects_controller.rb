c18e6366ae8e991150dc30bc448435b7e9fd4f40
Theodore A. Ochieng <royalato@gmail.com>
2018-01-21 22:23:16 +0000

    Added Update and Delete functionality for Projects. Also modified Create functionality
    Finished adding CRUD functionality for Projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
     
+    before_action :set_project, except: [:index, :new, :create]
+    
     def index
         @projects = Project.order("created_at DESC")
     end
@@ -7,19 +9,45 @@ class ProjectsController < ApplicationController
     def show
     end
     
-    #View for the create method
+    # View for the create method
     def new
         @project = Project.new
     end
     
     def create
         @project = Project.new(project_params)
-        @project.save
         
-        redirect_to root_path
+        if @project.save
+            redirect_to root_path
+        else
+            render 'new'
+        end
     end
     
+    # View for the update method
+    def edit
+    end
+    
+    def update
+        if @project.update(project_params)
+            redirect_to root_path, notice: "Successfully updated Project"
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @project.destroy
+        redirect_to root_path, notice: "Successfully deleted move"
+    end
+    
+    
     private
+    
+    # Method to find a specific Project using its ID
+    def set_project
+        @project = Project.find(params[:id])
+    end
     
     def project_params
         params.require(:project).permit(:title, :description)
